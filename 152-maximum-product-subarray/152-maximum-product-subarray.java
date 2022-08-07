d06f4938533af5e564aa4a27bf42e7b96ce6ef04
Samikshya Aryal <35891176+ikshyaa@users.noreply.github.com>
2022-08-07 19:27:20 -0400

    Time: 319 ms (5.02%), Space: 44.9 MB (50.78%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.java
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -1,39 +1,26 @@
 class Solution {
     public int maxProduct(int[] nums) {
         
-        if(nums.length ==1) return nums[0];
+        if(nums.length == 1) return nums[0];
         
-        int minProduct = nums[0];
+        
+        int n= nums.length;
+
         int maxProduct = nums[0];
-        int result = nums[0];
         
-        for(int i= 1; i<nums.length; i++) {
-            if(nums[i] == 0) {
-                minProduct =1;
-                maxProduct =1;
+        for(int i= 0; i<n; i++) {
+            int product = nums[i];
+        
+            for(int j =i+1; j< n; j++) {
+                
+                maxProduct = Math.max(maxProduct, product);
+                
+                product = product * nums[j];
+                
             }
+            maxProduct = Math.max(maxProduct, product);
             
-            int temp = maxProduct*nums[i];
-            maxProduct = Math.max((Math.max(maxProduct * nums[i], minProduct * nums[i])), nums[i]);
-            minProduct = Math.min((Math.min(temp, minProduct * nums[i])), nums[i]);
-            
-            result = Math.max(maxProduct, result);
         }
-        return result;
+        return maxProduct; 
     }
 }
-
-//             product = product * nums[i];
-            
-//             // if(product < nums[i]) product2 = nums[i];
-            
-//             product2 = Math.max(product, product2);
-//             for(int j =i; j< nums.length; j++) {
-//                 if(product2 < nums[j]) product2 = nums[i];
-//                 else product2 *= nums[j];
-//             }
-//             maxProduct = Math.max(maxProduct, product);
-            
-//             maxProduct = Math.max(maxProduct, product2); 
-//         }
-//         return maxProduct; 
