503394421de11d6f5328444ad9aef834a2156cd6
Samikshya Aryal <35891176+ikshyaa@users.noreply.github.com>
2022-08-07 15:50:11 -0400

    Time: 3 ms (53.87%), Space: 58 MB (39.17%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -17,11 +17,10 @@ class Solution {
         pp[0] = 1;
         int temp = 1; 
         
-        for(int i=0; i< nums.length; i++) {
-            if(i+1 < nums.length) {
-                pp[i+1] = pp[i] * nums[i];
+        for(int i=1; i< nums.length; i++) {
+                pp[i] = pp[i-1] * nums[i-1];
             }
-        }
+        
         
         for(int i = nums.length-1; i >= 0; i-- ) {
             pp[i] = pp[i] * temp; 
